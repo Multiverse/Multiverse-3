6e2cd8ab54352b983ed52ca33680ba6f0d94b9e3
Jeremy Wood <farachan@gmail.com>
2015-11-10 09:07:30 -0500

    Added non-localized constructors to TeleportException.

--- base/src/main/java/com/mvplugin/core/exceptions/TeleportException.java
+++ base/src/main/java/com/mvplugin/core/exceptions/TeleportException.java
@@ -2,8 +2,17 @@ package com.mvplugin.core.exceptions;
 
 import org.jetbrains.annotations.NotNull;
 import pluginbase.messages.BundledMessage;
+import pluginbase.messages.Message;
 
 public class TeleportException extends MultiverseException {
+
+    public TeleportException(@NotNull String message) {
+        super(Message.bundleMessage(Message.createStaticMessage(message)));
+    }
+
+    public TeleportException(@NotNull String message, @NotNull Throwable throwable) {
+        super(Message.bundleMessage(Message.createStaticMessage(message)), throwable);
+    }
 
     public TeleportException(@NotNull final BundledMessage languageMessage) {
         super(languageMessage);
